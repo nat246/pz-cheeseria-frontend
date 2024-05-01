6036d5eef2ba4ecb8a0ee4bf00fe12b1ee9ddd27
Nathanael Ang <natnatang@hotmail.com>
2024-05-02 08:03:32 +1000

    Add Tailwind CSS

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import SearchBar from "../components/SearchBar";
 const HomePage = () => {
   return (
     <div>
-      <h2>Discover your Cheese Nirvana</h2>
+      <h2 className="font-bold">Discover your Cheese Nirvana</h2>
 
       <SearchBar />
     </div>
