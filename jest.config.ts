635d2e8766fb78fae13f8b02254df6930b502a93
Nathanael Ang <natnatang@hotmail.com>
2024-05-03 01:03:13 +1000

    Add testing dependencies and updated readme

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
+export default config;
